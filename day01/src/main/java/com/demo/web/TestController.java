2d9802c24d5471158e9ca2b3499d618af1370c92
Raynor <1101732819@qq.com>
2020-02-13 20:52:30 +0800

    1

---
+++ day01/src/main/java/com/demo/web/TestController.java
@@ -0,0 +1,13 @@
+package com.demo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String index() {
+        return "Hello";
+    }
+}
