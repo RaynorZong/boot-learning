0c751152d97e558c52d65f367627888d28a47704
Raynor <1101732819@qq.com>
2020-02-18 16:50:10 +0800

    1

--- day04/src/main/java/com/demo/config/MvcConfig.java
+++ day04/src/main/java/com/demo/config/MvcConfig.java
@@ -1,47 +1,50 @@
-package com.demo03.config;
+package com.demo.config;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import lombok.extern.java.Log;
+import com.demo.interceptor.MyInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
-@Log
 
+@Slf4j
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer{
+    @Resource
+    MyInterceptor myInterceptor;
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/toLogin").setViewName("test.html");
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(myInterceptor).addPathPatterns("/**")
+                .excludePathPatterns("/toLogin","/login","/js/**","/css/**","/images/**");
+    }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        log.warning("====================================");
-
         registry.addResourceHandler("/my/**").addResourceLocations("classpath:/my/");
     }
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        log.warning("====================================");
 
-        /*
-         先把JackSon的消息转换器删除.
-         备注:
-         (1)源码分析可知，返回json的过程为: Controller调用结束后返回一个数据对象，for循环遍历conventers，找到支持application/json的HttpMessageConverter，然后将返回的数据序列化成json。
-34          具体参考org.springframework.web.servlet.mvc.method.annotation.AbstractMessageConverterMethodProcessor的writeWithMessageConverters方法
-35       (2)由于是list结构，我们添加的fastjson在最后。因此必须要将jackson的转换器删除，不然会先匹配上jackson，导致没使用fastjson
-        */
         for (int i = converters.size() - 1; i >= 0; i--) {
             if (converters.get(i) instanceof MappingJackson2HttpMessageConverter) {
                 converters.remove(i);
             }
         }
         FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
-        //自定义fastjson配置
         FastJsonConfig config = new FastJsonConfig();
         config.setSerializerFeatures(
                 SerializerFeature.WriteMapNullValue,        // 是否输出值为null的字段,默认为false,我们将它打开
@@ -52,9 +55,6 @@ public class MvcConfig implements WebMvcConfigurer {
                 SerializerFeature.DisableCircularReferenceDetect    // 禁用循环引用
         );
         fastJsonHttpMessageConverter.setFastJsonConfig(config);
-        // 添加支持的MediaTypes;不添加时默认为*/*,也就是默认支持全部
-        // 但是MappingJackson2HttpMessageConverter里面支持的MediaTypes为application/json
-        // 参考它的做法, fastjson也只添加application/json的MediaType
         List<MediaType> fastMediaTypes = new ArrayList<>();
         fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
         fastJsonHttpMessageConverter.setSupportedMediaTypes(fastMediaTypes);
