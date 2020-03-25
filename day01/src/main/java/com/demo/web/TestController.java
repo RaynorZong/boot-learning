94e434de2658897a4535c012d027b9bb5c9e6655
Raynor <1101732819@qq.com>
2020-03-25 22:22:35 +0800

    1

--- day01/src/main/java/com/demo/web/TestController.java
+++ day01/src/main/java/com/demo/web/TestController.java
@@ -1,13 +1,20 @@
 package com.demo.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class TestController {
 
     @RequestMapping("/test")
     public String index() {
-        return "Hello";
+        return "redirect:/test1";
+    }
+
+
+    @RequestMapping("/test1")
+    public @ResponseBody String inde1(){
+        return "sss";
     }
 }
